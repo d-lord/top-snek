b0273992ec35ca2d002f59a73ce175cca75fcaf1
David Lord <eternalcat@gmail.com>
2017-09-18 10:44:42 +1000

    Rewrite
    Flask-RESTful -> Flask + Marshmallow
    
    Add a dummy data endpoint for better local testing.

--- application.py
+++ application.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, marshal, fields
-from json import dumps
+from flask import Flask, request, jsonify
+from flask.views import MethodView
+import json.decoder
 import sqlalchemy
-from sqlalchemy import create_engine, Column#, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from pytz import timezone
+from marshmallow import Schema, fields, pprint
 import config
 import ipdb
 
 application = Flask(__name__)
-api = Api(application)
 
-request_parser = reqparse.RequestParser()
-request_parser.add_argument('id', help="Slack user ID", required=True)
-request_parser.add_argument('name', help="Slack username (mutable)", required=True)
-request_parser.add_argument('story_count', type=int, default=0, help="# of stories told")
-# request_parser.add_argument('story_count', type=Date, default=None, help="# of stories told")
-
-db_engine = create_engine(config.db_file) # eugh... validate ../?
+db_engine = sqlalchemy.create_engine(config.db_file)
 Session = sessionmaker(bind=db_engine)
 
 Base = declarative_base()
@@ -28,55 +23,58 @@ class User(Base):
     __tablename__ = 'users'
 
     # SQLAlchemy schema
-    id = Column(sqlalchemy.String, primary_key=True)
-    name = Column(sqlalchemy.String, nullable=False)
-    story_count = Column(sqlalchemy.Integer, default=0)
-    last_story = Column(sqlalchemy.Date, default=None)
+    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    story_count = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    last_story = sqlalchemy.Column(sqlalchemy.DateTime, default=None)
 
-    # Flask-RESTful schema
-    resource_fields = {
-            'name': fields.String,
-            'id': fields.String,
-            'story_count': fields.Integer,
-            'last_story': fields.DateTime
-    }
+    # marshmallow schema
+    # TODO: required attrs? or let sqlalchemy enforce that?
+    # also, can we make this class static somehow? save User.Schema()
+    class Schema(Schema):
+        name = fields.Str()
+        id = fields.Str()
+        story_count = fields.Int()
+        last_story = fields.DateTime()
 
     def __repr__(self):
-        return "<User(name='%s', id='%s', story_count='%d', last_story='%s'" % (self.name, self.id, self.story_count, self.last_story)
+        return str(self.Schema().dump(self).data)
 
 
 # TODO: middleware to ensure valid team token
-class Leaderboard(Resource):
+class Leaderboard(MethodView):
     def get(self):
         session = Session()
-        return [marshal(user, User.resource_fields) for user in session.query(User).order_by(User.story_count.desc()).all()]
+        schema = User.Schema()
+        return jsonify([schema.dump(user).data for user in session.query(User).order_by(User.story_count.desc()).all()])
 
-# a Flask resource (distinct from the sqlalchemy User)
-class APIUser(Resource):
-    # create
+# a Flask endpoint (distinct from, but corresponding to, the sqlalchemy User)
+# TODO: ensure valid administrator is making the request (middleware?)
+class APIUser(MethodView):
     def post(self):
-        # TODO: ensure valid administrator is making the request (middleware?)
         try:
             session = Session()
-            values = request_parser.parse_args(strict=True)
-            new_user = User(name=values["name"], id=values["id"],
-                    story_count = values["story_count"])
-            ipdb.set_trace()
-            session.add(new_user)
+            result = User.Schema(strict=True).load(request.get_json(force=True))
+            # check for error
+            if len(result.errors):
+                return (jsonify(result.errors), 400)
+            new_user = User(**result.data)
+            session.add(new_user) 
+            # test exists for simpler error message? otherwise it's "(sqlite3.IntegrityError) UNIQUE constraint failed: users.id"
             session.commit()
         except (sqlalchemy.exc.IntegrityError, sqlalchemy.exc.InvalidRequestError) as e:
             return (e.args[0], 400)
-    # maybe it should have PUT etc too. any way of hooking up CRUD via sqlalchemy?
+        return (b'', 201)
+    # maybe it should have PUT etc too. any way of hooking up CRUD via sqlalchemy & marshmallow?
+    # it needs /something/ for "told_story", maybe "/users/<id>/told_story"
 
-class CreateDummyData(Resource):
+
+class CreateDummyData(MethodView):
     def post(self):
         session = Session()
         created, skipped = 0, 0
-        for user_data in [
-                {"name": "dal", "id": "LOLJK", "story_count": 5},
-                {"name": "steve", "id": "BIGMAN", "story_count": 4},
-                {"name": "jaina", "id": "ICE422", "story_count": 0}]:
-            new_user = User(**user_data)
+        for raw_user in config.sample_users:
+            new_user = User(**raw_user)
             query = session.query(User).filter(User.id == new_user.id)
             if session.query(query.exists()).scalar():
                 skipped += 1
@@ -84,12 +82,12 @@ class CreateDummyData(Resource):
                 session.add(new_user)
                 created += 1
         session.commit()
-        return ({"created": created, "skipped": skipped}, 200)
+        return (jsonify({"created": created, "skipped": skipped}), 200)
 
 
-api.add_resource(Leaderboard, '/')
-api.add_resource(APIUser, '/users')
-api.add_resource(CreateDummyData, '/create_dummy_data')
+application.add_url_rule('/', view_func=Leaderboard.as_view('leaderboard'))
+application.add_url_rule('/users', view_func=APIUser.as_view('users'))
+application.add_url_rule('/create_dummy_data', view_func=CreateDummyData.as_view('create'))
 
 if __name__ == '__main__':
     # ipdb.set_trace()
--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 db_file = "sqlite:///users.db"
+
+sample_users = [
+        {"name": "dal", "id": "LOLJK", "story_count": 5},
+        {"name": "steve", "id": "BIGMAN", "story_count": 4},
+        {"name": "jaina", "id": "ICE422", "story_count": 0}
+]
